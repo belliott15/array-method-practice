417c471cadbe056625439bb82944517074d3a841
Beau Elliott <belliott1516@gmail.com>
2023-01-19 09:27:55 -0800

    finished all tests and passing

--- reduce.js
+++ reduce.js
@@ -2,9 +2,17 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce((acc, curr) => [...acc, ...curr.shoes], []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  const reduced = dogs.reduce((acc, dog) => {
+    acc += dog.age;
+    return acc;
+  }, 0);
+  return reduced / dogs.length;
+};
